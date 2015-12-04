7682bf65e6f54622d94caf46e20b5097287f9d8d
Lorenz Schimik <losc13@gmail.com>
2015-12-04 17:45:38 +0100

    password reset added

---
+++ app/controllers/users_controller.js
@@ -0,0 +1,131 @@
+//load some stuff
+var async       = require('async');
+var crypto      = require('crypto');
+var bcrypt      = require('bcrypt-nodejs');
+var mongoose    = require('mongoose');
+var User        = mongoose.model('User'); 
+var nodemailer  = require('nodemailer');
+
+//helper functions
+//encrypt the password after a password reset
+function generateHash(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+
+
+//give the user the chance to set a new password ============================
+exports.forgotPassword = function(req,res, next){
+    async.waterfall([
+      function(done) {
+        crypto.randomBytes(20, function(err, buf) {
+          var token = buf.toString('hex');
+          done(err, token);
+        });
+      },
+      function(token, done) {
+        User.findOne({ email: req.body.email }, function(err, user) {
+          if (!user) {
+            req.flash('newPasswordMessage', 'No account with that email address exists.');
+            return res.redirect('/forgot');
+          }
+
+          user.resetPasswordToken = token;
+          user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+          user.save(function(err) {
+            done(err, token, user);
+          });
+        });
+      },
+      function(token, user, done) {
+        var smtpTransport = nodemailer.createTransport('SMTP', {
+          service: 'Gmail',
+          auth: {
+            user: 'email',
+            pass: 'password'
+          }
+        });
+        var mailOptions = {
+          to: user.email,
+          from: 'email',
+          subject: 'Airdate Password Reset',
+          text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+            'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+            'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+        };
+        smtpTransport.sendMail(mailOptions, function(err) {
+          req.flash('newPasswordMessage', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+          done(err, 'done');
+        });
+      }
+    ], function(err) {
+      if (err) return next(err);
+      res.redirect('/forgot');
+    });
+};
+
+//user clicked on reset password link in email
+exports.renderResetPassword = function(req,res){
+	User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+	  if (!user) {
+	    req.flash('error', 'Password reset token is invalid or has expired.');
+	    return res.redirect('/forgot');
+	  }
+	  res.render('reset.html', {
+	    user: req.user,
+	    message: req.flash()
+	  });
+	});
+};
+
+
+//user sets new password ====================================
+exports.resetPassword = function(req,res, next){
+    async.waterfall([
+      function(done) {
+        User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+          if (!user) {
+            req.flash('error', 'Password reset token is invalid or has expired.');
+            return res.redirect('back');
+          }
+
+          user.password = generateHash(req.body.password);
+          user.resetPasswordToken = undefined;
+          user.resetPasswordExpires = undefined;
+
+          user.save(function(err) {
+            req.logIn(user, function(err) {
+              done(err, user);
+            });
+          });
+        });
+      },
+      function(user, done) {
+        var smtpTransport = nodemailer.createTransport('SMTP', {
+          service: 'Gmail',
+          auth: {
+            user: 'email',
+            pass: 'password'
+          }
+        });
+        var mailOptions = {
+          to: user.email,
+          from: 'email',
+          subject: 'Your Airdate password has been changed',
+          text: 'Hello,\n\n' +
+            'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+        };
+        smtpTransport.sendMail(mailOptions, function(err) {
+          req.flash('success', 'Success! Your password has been changed.');
+          done(err);
+        });
+      }
+    ], function(err) {
+      res.redirect('/app');
+    });
+};
+
+
+
